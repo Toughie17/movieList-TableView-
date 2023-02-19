b5c37af6e092636f2b7f3f71c0ebcef0a05f7545
Toughie <airj907@gmail.com>
2023-02-19 16:39:48 +0900

    First commit

---
+++ tableViewProject(CODE)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableViewProject(CODE)
+//
+//  Created by KimChoonSik on 2023/02/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
